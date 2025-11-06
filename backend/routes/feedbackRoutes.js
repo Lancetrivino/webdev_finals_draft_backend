c8be98dbf30f56044ac9e50fb92ed39285fd8755
Lance <lancetrivino30@gmail.com>
2025-11-07 01:30:27 +0800

    wrong again

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { submitFeedback, getFeedbackByEvent } from "../controllers/feedbackController.js"; // ✅ include both
+import { addFeedback, getFeedbackByEvent } from "../controllers/feedbackController.js"; // ✅ include both
 
 const router = express.Router();
 
 // Add feedback for an event
-router.post("/:id", protect, submitFeedback);
+router.post("/:id", protect, addFeedback);
 
 // Get all feedback for a specific event
 router.get("/:id", protect, getFeedbackByEvent);
