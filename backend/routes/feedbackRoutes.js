4217e946c7a283f1405609b67937b59f85abd9a0
Lance <lancetrivino30@gmail.com>
2025-11-07 01:28:14 +0800

    change of adfeedback to submitfeedback

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { addFeedback, getFeedbackByEvent } from "../controllers/feedbackController.js"; // ✅ include both
+import { submitFeedback, getFeedbackByEvent } from "../controllers/feedbackController.js"; // ✅ include both
 
 const router = express.Router();
 
 // Add feedback for an event
-router.post("/:id", protect, addFeedback);
+router.post("/:id", protect, submitFeedback);
 
 // Get all feedback for a specific event
 router.get("/:id", protect, getFeedbackByEvent);
